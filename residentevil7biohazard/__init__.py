473985ef15feb817fb7b6298ce7cc153d41aa4a8
ElGrenier <48546541+ElGrenier@users.noreply.github.com>
2025-11-22 23:08:34 +0100

    Update __init__.py

--- residentevil7biohazard/__init__.py
+++ residentevil7biohazard/__init__.py
@@ -36,7 +36,7 @@ class ResidentEvil7(World):
 
     data_version = 2
     required_client_version = (0, 4, 4)
-    apworld_release_version = "0.2.0" # defined to show in spoiler log
+    apworld_release_version = "0.2.1" # defined to show in spoiler log
 
     item_id_to_name = { item['id']: item['name'] for item in Data.item_table }
     item_name_to_id = { item['name']: item['id'] for item in Data.item_table }
