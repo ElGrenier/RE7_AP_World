35d04bf8b524839ec2a5617facb000a50b9dfa8f
ElGrenier <48546541+ElGrenier@users.noreply.github.com>
2025-11-22 23:36:40 +0100

    Fix?

--- residentevil7biohazard/Data.py
+++ residentevil7biohazard/Data.py
@@ -23,8 +23,8 @@ class Data:
     item_name_groups = {}
 
     def load_data():
-        #hardcore_offset = 400 # put all hardcore-only locations in the last 100 location spots for each scenario
-        #scenario_suffix_hardcore = ' ({}{}H)' # makes hardcore location variations unique
+        hardcore_offset = 400 # put all hardcore-only locations in the last 100 location spots for each scenario
+        scenario_suffix_hardcore = ' ({}{}H)' # makes hardcore location variations unique
 
         location_start = item_start = 3000000000
 
@@ -51,7 +51,7 @@ class Data:
         if len(hardcore_regions) > 0:
             Data.region_table.extend([
                 {
-                    'name': reg, # add the scenario abbreviation so they're unique
+                    'name': reg + scenario_suffix_hardcore, # add the scenario abbreviation so they're unique
                     'zone_id': [regular['zone_id'] for regular in new_region_table if regular['name'] == reg][0]
                 }
                 for reg in hardcore_regions # instead of using region definitions, we're using the hardcore region additions from the locations themselves
@@ -85,28 +85,28 @@ class Data:
         ###
 
         # not a typo. if we loaded hardcore regions, we need to generate hardcore region connections as well
-        # if len(hardcore_regions) > 0:
-        #     for conn in new_region_connections_table:
-        #         if conn['from'] in hardcore_regions or conn['to'] in hardcore_regions:
-        #             # suffix_from = scenario_suffix_hardcore if conn['from'] in hardcore_regions else scenario_suffix
-        #             # suffix_to = scenario_suffix_hardcore if conn['to'] in hardcore_regions else scenario_suffix
+        if len(hardcore_regions) > 0:
+            for conn in new_region_connections_table:
+                if conn['from'] in hardcore_regions or conn['to'] in hardcore_regions:
+                    # suffix_from = scenario_suffix_hardcore if conn['from'] in hardcore_regions else scenario_suffix
+                    # suffix_to = scenario_suffix_hardcore if conn['to'] in hardcore_regions else scenario_suffix
 
-        #             connection_from_name = conn['from']
-        #             connection_to_name = conn['to']
-        #             connection_path = f"{connection_from_name} to {connection_to_name}"
+                    connection_from_name = conn['from']
+                    connection_to_name = conn['to']
+                    connection_path = f"{connection_from_name} to {connection_to_name}"
 
-        #             if connection_path in added_connections:
-        #                 continue
+                    if connection_path in added_connections:
+                        continue
 
-        #             added_connections.append(connection_path)
+                    added_connections.append(connection_path)
 
-        #             new_region_connection = {
-        #                 **conn,
-        #                 'from': connection_from_name, 
-        #                 'to': connection_to_name, 
-        #             }
+                    new_region_connection = {
+                        **conn,
+                        'from': connection_from_name, 
+                        'to': connection_to_name, 
+                    }
 
-        #             Data.region_connections_table.append(new_region_connection)
+                    Data.region_connections_table.append(new_region_connection)
 
         ###
         # Add item table for all difficulties
@@ -159,8 +159,8 @@ class Data:
             Data.location_table.extend([
                 { 
                     **loc, 
-                    'id': loc['id'] if loc.get('id') else location_start + key,
-                    'region': loc['region'] , # add the scenario abbreviation so they're unique
+                    'id': loc['id'] if loc.get('id') else location_start + key + hardcore_offset,
+                    'region': loc['region'] + scenario_suffix_hardcore, # add the scenario abbreviation so they're unique
                     'difficulty': 'hardcore'
                 }
                 for key, loc in enumerate(hardcore_location_table)
